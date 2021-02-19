4656fa978b15dda633be33bec730f6e56a8b0269
Alexenfeu <alexissarlat@gmail.com>
2021-02-19 17:53:10 +0100

    initial commit

---
+++ DM_JDR_Console/DM_JDR_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM_JDR_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
