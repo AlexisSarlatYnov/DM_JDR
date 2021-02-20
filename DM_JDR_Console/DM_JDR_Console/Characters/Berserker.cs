128b01dda8e6af6f71f1311601ce9a0c1d5ec821
Alexenfeu <alexissarlat@gmail.com>
2021-02-20 12:32:26 +0100

    Berserker in progress

---
+++ DM_JDR_Console/DM_JDR_Console/Characters/Berserker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM_JDR_Console.Characters
+{
+    class Berserker : Character
+    {
+
+        public int lostLifeInAHit = 0;
+        public int lostLifeTotal = 0;
+        public float initialAttackSpeed = 1f;
+        public Berserker()
+        {
+            this.attack = 50;
+            this.defense = 50;
+            this.attackSpeed = 1.1f;
+            this.damages = 20;
+            this.maximumLife = 400;
+            this.currentLife = 400;
+            this.powerSpeed = 1f;
+            this.lostLifeInAHit = 0;
+            this.lostLifeTotal = 0;
+        }
+
+        public int GetLostLifeInAHit()
+        {
+            return this.lostLifeInAHit;
+        }
+
+        public void SetLostLifeInAHit(int pLostLifeInAHit)
+        {
+            this.lostLifeInAHit = pLostLifeInAHit;
+        }
+
+        public int GetLostLifeTotal()
+        {
+            return this.lostLifeTotal;
+        }
+
+        public void SetLostLifeTotal(int pLostLifeTotal)
+        {
+            this.lostLifeTotal = pLostLifeTotal;
+        }
+
+        public void BerserkerPower()
+        {
+            this.SetAttack(this.GetAttack() + (int)Math.Round(this.GetLostLifeInAHit() / 2f));
+            this.SetDefense(this.GetDefense() + (int)Math.Round(this.GetLostLifeInAHit() / 2f));
+            int dixPourcentMaxLife = (int)(this.GetMaximumLife() * 0.1f);
+            
+        }
+
+
+
+        //TO DO Passif
+    }
+}
--- DM_JDR_Console/DM_JDR_Console/Program.cs
+++ DM_JDR_Console/DM_JDR_Console/Program.cs
@@ -11,13 +11,22 @@ namespace DM_JDR_Console
     {
         static void Main(string[] args)
         {
-            Character charac1 = new Character();
-            Character charac2 = new Character();
+            Test();
+            
+        }
+
+        public static void Test()
+        {
+            Berserker charac1 = new Berserker();
+            Berserker charac2 = new Berserker();
             Console.WriteLine("Attack du charac 1 : " + charac1.GetAttack().ToString());
             Console.WriteLine("Attack du charac 2 : " + charac2.GetAttack().ToString());
+            Console.WriteLine("Vie perdue en une attaque du charac 1 : " + charac1.GetLostLifeInAHit().ToString());
             charac1.SetAttack(2);
+            charac1.SetLostLifeInAHit(100);
             Console.WriteLine("Attack du charac 1 : " + charac1.GetAttack().ToString());
             Console.WriteLine("Attack du charac 2 : " + charac2.GetAttack().ToString());
+            Console.WriteLine("Vie perdue en une attaque du charac 1 : " + charac1.GetLostLifeInAHit().ToString());
             Console.ReadLine();
         }
     }
